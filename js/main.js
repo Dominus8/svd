ae0f90413ae0e08f4b8408a769cbecd2c1f6cd1a
Dominus8 <ikari162@mail.ru>
2022-02-14 15:54:09 +0300

    Переделал один слайдер на два и прикрутил переключение слайдов к кнопкам меню.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-const swiper = new Swiper('.swiper', {
+let swiperTop = new Swiper('.swiper-top', {
     // Optional parameters
+    effect: 'fade',
+    // slidesPerView: 1,
     direction: 'vertical',
-    loop: true,
+    loop: false,
+    mousewheel: true,
+    allowTouchMove: true,
+
+    // Navigation arrows
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // }
+});
+
+let SwiperBot = new Swiper('.swiper-bot', {
+    // Optional parameters
+    // slidesPerView: 1,
+    direction: 'vertical',
+    loop: false,
     mousewheel: true,
 
     // Navigation arrows
@@ -9,4 +26,43 @@ const swiper = new Swiper('.swiper', {
     //     nextEl: '.swiper-button-next',
     //     prevEl: '.swiper-button-prev',
     // }
+});
+
+swiperTop.controller.control = SwiperBot;
+SwiperBot.controller.control = swiperTop;
+
+$('.tooslide1').click(function(e) {
+
+    e.preventDefault();
+    $(".nav .nav_el--active").removeClass('nav_el--active');
+    $(this).addClass('nav_el--active');
+    swiperTop.slideTo($('.about-st').index(), 700, false);
+});
+
+$('.tooslide2').click(function(e) {
+    e.preventDefault();
+    $(".nav .nav_el--active").removeClass('nav_el--active');
+    $(this).addClass('nav_el--active');
+    swiperTop.slideTo($('.we_can-st').index(), 700, false);
+});
+
+$('.tooslide3').click(function(e) {
+    e.preventDefault();
+    $(".nav .nav_el--active").removeClass('nav_el--active');
+    $(this).addClass('nav_el--active');
+    swiperTop.slideTo($('.services-st').index(), 700, false);
+});
+
+$('.tooslide4').click(function(e) {
+    e.preventDefault();
+    $(".nav .nav_el--active").removeClass('nav_el--active');
+    $(this).addClass('nav_el--active');
+    swiperTop.slideTo($('.contacts-st').index(), 700, false);
+});
+
+$('.tooslide5').click(function(e) {
+    e.preventDefault();
+    $(".nav .nav_el--active").removeClass('nav_el--active');
+    $(this).addClass('nav_el--active');
+    swiperTop.slideTo($('.partners-st').index(), 700, false);
 });
